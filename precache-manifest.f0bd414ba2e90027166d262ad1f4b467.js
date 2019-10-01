1c9dbe107e7a8e0c685b8fe63e21ada605f510f8
Anelvin <amejiaroque@gmail.com>
2019-10-01 15:44:25 +0200

    Updates

---
+++ precache-manifest.f0bd414ba2e90027166d262ad1f4b467.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a150bd084aa924d4d06fa5cda51ba640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f61817e3d6f03fe385a",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "cd78ef7b53e35a9489d5",
+    "url": "/static/js/2.16307587.chunk.js"
+  },
+  {
+    "revision": "9f61817e3d6f03fe385a",
+    "url": "/static/js/main.a5557b55.chunk.js"
+  },
+  {
+    "revision": "65a81b1ca9a7620b8ceb",
+    "url": "/static/js/runtime-main.1dc4db23.js"
+  }
+]);
