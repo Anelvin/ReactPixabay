e3361df8f045208fbc05681b00e022c4199fa6e3
Anelvin <amejiaroque@gmail.com>
2019-10-01 15:54:41 +0200

    Updates

---
+++ precache-manifest.4b597fa919a3919604573ed5b98d595e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223cddbac4b7044e199e9c55a3798438",
+    "url": "/ReactPixabay/index.html"
+  },
+  {
+    "revision": "4afa292fca153b4c9bce",
+    "url": "/ReactPixabay/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "cd78ef7b53e35a9489d5",
+    "url": "/ReactPixabay/static/js/2.16307587.chunk.js"
+  },
+  {
+    "revision": "4afa292fca153b4c9bce",
+    "url": "/ReactPixabay/static/js/main.b742d76f.chunk.js"
+  },
+  {
+    "revision": "2a9bbf80e663e1ea2409",
+    "url": "/ReactPixabay/static/js/runtime-main.32c7b17f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f0bd414ba2e90027166d262ad1f4b467.js"
+  "/ReactPixabay/precache-manifest.4b597fa919a3919604573ed5b98d595e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactPixabay/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
